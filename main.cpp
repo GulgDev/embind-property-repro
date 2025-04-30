f8dc86e2d6844ce159e25e939bcd5b363f313917
GulgDev <65360617+GulgDev@users.noreply.github.com>
2025-05-01 00:12:53 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <emscripten/bind.h>
+
+using namespace emscripten;
+
+class Thing {
+public:
+    int value;
+
+    Thing() : value(0) {}
+};
+
+class MyClass {
+public:
+    Thing *thing;
+
+    Thing* getThing() {
+        return thing;
+    }
+
+    void setThing(Thing* thing) {
+        thing = thing;
+    }
+};
+
+EMSCRIPTEN_BINDINGS(Example) {
+    class_<Thing>("Thing")
+        .constructor()
+        .property("value", &Thing::value);
+
+    class_<MyClass>("MyClass")
+        .property("thing", // error: implicit instantiation of undefined template
+                  &MyClass::getThing,
+                  &MyClass::setThing,
+                  allow_raw_pointers(),
+                  return_value_policy::reference());
+}
